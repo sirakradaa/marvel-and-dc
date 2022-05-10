3e10ed8148e9658904d7e73e36a013544bb18c47
Sirak Radaa <radaa.sirak@gmail.com>
2022-05-10 02:49:51 -0600

    Picked heroes/villains for DC, added a little mobile responsiveness to dc.jsx

--- src/components/dc/dc.jsx
+++ src/components/dc/dc.jsx
@@ -8,6 +8,8 @@ import { dcChars } from './../../images/dc-images';
 // DC side which displays heros on the right and villains on the left
 // with a description of the character, affiliations, opponents and rank
 const dc = () =>  {
+
+
   return (
     <AnimationsPage>
       <div className = "dc-container">
@@ -18,9 +20,20 @@ const dc = () =>  {
                 <p className="legend">Batman</p>
                 <div className="dc-desc-hero">HERE</div>
             </div>
-            <div>
-                <img src="assets/2.jpeg" alt="" />
-                <p className="legend">Legend 2</p>
+            <div className="dc-image-container-hero">
+                <img className="dc-img-hero" src={ dcChars.Superman } alt="" />
+                <p className="legend">Superman</p>
+                <div className="dc-desc-hero">HERE</div>
+            </div>
+            <div className="dc-image-container-hero">
+                <img className="dc-img-hero" src={ dcChars.Wonderwoman } alt="" />
+                <p className="legend">Wonder Woman</p>
+                <div className="dc-desc-hero">HERE</div>
+            </div>
+            <div className="dc-image-container-hero">
+                <img className="dc-img-hero" src={ dcChars.Flash } alt="" />
+                <p className="legend">Flash</p>
+                <div className="dc-desc-hero">HERE</div>
             </div>
         </Carousel>
         {/*Villain carousel*/}
@@ -30,9 +43,20 @@ const dc = () =>  {
               <div className="dc-desc-villain">HERE</div>
               <img className="dc-img-villain" src={ dcChars.Joker } alt="" />
           </div>
-          <div>
-              <img src="assets/2.jpeg" alt="" />
-              <p className="legend">Legend 2</p>
+          <div className="dc-image-container-villain">
+              <p className="legend">Lex Luthor</p>
+              <div className="dc-desc-villain">HERE</div>
+              <img className="dc-img-villain" src={ dcChars.Lexluthor } alt="" />
+          </div>
+          <div className="dc-image-container-villain">
+              <p className="legend">Darkseid</p>
+              <div className="dc-desc-villain">HERE</div>
+              <img className="dc-img-villain" src={ dcChars.Darkseid } alt="" />
+          </div>
+          <div className="dc-image-container-villain">
+              <p className="legend">Bane</p>
+              <div className="dc-desc-villain">HERE</div>
+              <img className="dc-img-villain" src={ dcChars.Bane } alt="" />
           </div>
         </Carousel>
       </div>
--- src/images/dc-images.js
+++ src/images/dc-images.js
@@ -1,9 +1,19 @@
 import Batman from "./DC/batman.png"
 import Joker from "./DC/joker.png"
 import Superman from "./DC/superman.jpg"
+import Wonderwoman from "./DC/wonderwoman.jpg"
+import Flash from "./DC/flash.jpg"
+import Darkseid from "./DC/darkseid.jpeg"
+import Lexluthor from "./DC/lexluthor.jpg"
+import Bane from "./DC/bane.jpg"
 
 export const dcChars = {
     Batman,
     Joker,
-    Superman
+    Superman,
+    Wonderwoman,
+    Flash,
+    Darkseid,
+    Lexluthor,
+    Bane,
 };
