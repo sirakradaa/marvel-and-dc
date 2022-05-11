7bd264bc38cc8b0e99eebca9bb6cac2e6a042345
Sirak Radaa <radaa.sirak@gmail.com>
2022-05-10 22:18:36 -0600

    Fixed DC thumbnail/picture size, changed some images

--- src/images/dc-images.js
+++ src/images/dc-images.js
@@ -1,9 +1,9 @@
-import Batman from "./DC/batman.png"
+import Batman from "./DC/batman.jpg"
 import Joker from "./DC/joker.png"
 import Superman from "./DC/superman.jpg"
-import Wonderwoman from "./DC/wonderwoman.jpg"
+import Wonderwoman from "./DC/wonderwoman.png"
 import Flash from "./DC/flash.jpg"
-import Darkseid from "./DC/darkseid.jpeg"
+import Darkseid from "./DC/darkseid.jpg"
 import Lexluthor from "./DC/lexluthor.jpg"
 import Bane from "./DC/bane.jpg"
 
