106a9fd1189c9c65346fb58ecd55cab9539f60a4
Sirak Radaa <radaa.sirak@gmail.com>
2022-05-11 20:42:39 -0600

    Working vote form that only shows unselected options with the option to select top 8

--- src/components/vote/vote.jsx
+++ src/components/vote/vote.jsx
@@ -1,30 +1,95 @@
 import React from 'react'
+import { useState } from 'react'
 import AnimationsPage from './../AnimationsPage';
 import './vote.css'
 
-const vote = () =>  {
+const Vote = () =>  {
+
+  // number of select boxes
+  const selectNames = ["select1", "select2","select3", "select4","select5", 
+  "select6","select7", "select8"];
+
+  // options of characters
+  const [options] = useState([
+    {
+        label: 'Bane',
+        value: 'bane'
+    },
+    {
+        label: 'Batman',
+        value: 'batman'
+    },
+    {
+        label: 'Darkseid',
+        value: 'darkseid'
+    },    
+    {
+        label: 'Flash',
+        value: 'flash'
+    },
+    {
+        label: 'Joker',
+        value: 'joker'
+    },
+    {
+        label: 'Lex Luthor',
+        value: 'lex'
+    },    
+    {
+        label: 'Superman',
+        value: 'superman'
+    },
+    {
+        label: 'Wonder Woman',
+        value: 'wonder'
+    }
+  ]);
+
+  // options that are currently picked
+  const [chosenOptions, setChosenOptions] = useState({});
+
+  // check if another box has the option selected
+  const isChosenByOther = (optionValue, selectName) => {
+    for (let key in chosenOptions) {
+        if (key !== selectName) {
+            if (chosenOptions[key] === optionValue) {
+                return true;
+            }
+        }
+    }
+    return false;
+  };
+
+  const handleChange = (e) => {
+    setChosenOptions({...chosenOptions, [e.target.name]: e.target.value});
+  };
+
   return (
     <AnimationsPage>
       <div className="vote-container">
         <div className="ranking">
-        <form action="/action_page.php">
-          <label for="characters">Most to least favourite: </label>
-          <select id="characters" name="characters">
-            <option value="Batman">Batman</option>
-            <option value="Superman">Superman</option>
-            <option value="Wonder">Wonder Woman</option>
-            <option value="Flash">Flash</option>
-            <option value="Batman">Joker</option>
-            <option value="Lex">Lex Luthor</option>
-            <option value="Darkseid">Darkseid</option>
-            <option value="Bane">Bane</option>
-          </select>
-          <input type="submit"/>
-        </form>
+          <form action="/action_page.php">
+            <label htmlFor="characters">Pick your top 8 </label>
+            {selectNames.map((name, index) => {
+                  return (
+                    <div className="select-container">
+                      <select name={name} key={index} onChange={handleChange} 
+                      value={chosenOptions[name]} required={index === 0}>
+                        <option value=''/>
+                        {options.filter(({value}) => !isChosenByOther(value, name))
+                            .map(({label, value}) =>
+                                <option value={value} key={value}>{label}</option>)
+                        }
+                      </select>
+                    </div>
+                  )
+              })}
+            <input type="submit"/>
+          </form>
         </div>
       </div>
     </AnimationsPage>
   )
 }
 
-export default vote
+export default Vote
