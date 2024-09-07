93d6bddc4cd0e34fb64b4ec654f8779c158ff507
Sirak Radaa <60719924+sirakradaa@users.noreply.github.com>
2024-09-07 03:16:07 -0600

    Updates

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { recalculateReducer } from './recalculate';
-import { votesReducer } from './votes';
-import { combineReducers } from 'redux';
-
-export const allReducers = combineReducers({
-    recalculate: recalculateReducer,
-    votes: votesReducer
-})
--- src/reducers/votes.js
+++
@@ -1,9 +0,0 @@
-// Increase the number of votes by 1
-export const votesReducer = (state = 0, action) =>{
-    switch(action.type){
-        case 'INCREMENT':
-            return state + 1;
-        default:
-            return state;
-    }
-};
